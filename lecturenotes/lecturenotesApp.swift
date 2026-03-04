7bf7f2b56cca11d1c6a5b27430b6e9962d7c81af
Marat Sadykov <m.sadykovdev@gmail.com>
2026-03-04 23:30:41 +0500

    Initial Commit

---
+++ lecturenotes/lecturenotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  lecturenotesApp.swift
+//  lecturenotes
+//
+//  Created by Marat Sadykov on 04.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct lecturenotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
